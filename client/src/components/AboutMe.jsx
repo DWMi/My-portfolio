dd3bead66cceb94f8512000e5683753f5d876b53
David Wong <90611100+DWMi@users.noreply.github.com>
2023-04-14 18:11:26 +0200

    Update AboutMe.jsx

--- client/src/components/AboutMe.jsx
+++ client/src/components/AboutMe.jsx
@@ -10,7 +10,7 @@ const AboutMe = () => {
         <h2 style={{margin:'20px',color:'white'}}><span style={{color:'rgb(108, 182, 250)', fontSize:'30px'}}>D</span>avid <span style={{color:'rgb(108, 182, 250)', fontSize:'30px'}}>W</span>ong</h2>
         <p className="aboutMeText">
         <img className="proPic" src={proPic}/>
-        I'm born and raised in Örebro and I moved to Gothenburg in the begining of 2019 to work here as a chef/cook and certain things happened and i decided to change my career to be a Web-developer. <br/>Right now I'm almost finished with my studies at Medieinstitutet as a Fullstack developer <em>("17th May is the graduation")</em>.  I'm living right now with my brother and my two Cats Nikita and Mini. In my free time i like to go fishing in lakes, or just being in the nature if the weather is nice, otherwise I'll just be indoors infront of my PC.
+        I'm born and raised in Örebro and I moved to Gothenburg in the beginning of 2019 to work here as a chef/cook and certain things happened and i decided to change my career to be a Web-developer. <br/>Right now I'm almost finished with my studies at Medieinstitutet as a Fullstack developer <em>("17th May is the graduation")</em>.  I'm living right now with my brother and my two Cats Nikita and Mini. In my free time i like to go fishing in lakes, or just being in the nature if the weather is nice, otherwise I'll just be indoors infront of my PC.
         
         </p>
       </div>
